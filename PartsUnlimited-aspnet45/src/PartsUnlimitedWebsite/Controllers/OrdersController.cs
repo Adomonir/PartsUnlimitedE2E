dcbbe0b09d77a8245a1702aef11c807886db3a2f
Adeola <52584303+Adomonir@users.noreply.github.com>
2021-05-05 15:42:23 -0400

    Update OrdersController.cs
    AB#10053

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Controllers/OrdersController.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Controllers/OrdersController.cs
@@ -11,6 +11,7 @@ using PartsUnlimited.ViewModels;
 namespace PartsUnlimited.Controllers
 {
     [Authorize]
+    //AO
     public class OrdersController : Controller
     {
         private readonly IOrdersQuery _ordersQuery;
