341fde16faa62f51860db2a7564b2d72382bda89
Adeola Omonira <adomonir@microsoft.com>
2021-05-12 14:26:23 -0400

    Updated Account Controller

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Controllers/AccountController.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Controllers/AccountController.cs
@@ -11,7 +11,7 @@ using PartsUnlimited.Models;
 namespace PartsUnlimited.Controllers
 {
     [Authorize]
-    //my second change 1
+    //my third change
     public class AccountController : Controller
     {
         private SignInManager<ApplicationUser, string> SignInManager
